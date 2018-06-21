c071958f5949d8250a7fbca51651539e679079d2
KonathalaVenkataChinniKrishna <chinni.konathala@gmail.com>
2018-06-21 16:07:34 +0530

    small change

--- src/app/bargraph/bargraph.component.ts
+++ src/app/bargraph/bargraph.component.ts
@@ -22,6 +22,23 @@ export class BargraphComponent {
   data: any;
   @ViewChild('nvd3') nvd3;
 
+  columnDefs = [
+    {headerName: 'User', field: 'user', width: 250 },
+    {headerName: 'Type', field: 'type' },
+    {headerName: 'Counter Part', field: 'counter', width: 250 },
+    {headerName: 'Data', field: 'data' },
+    {headerName: 'Time Stamp', field: 'time'}
+];
+
+rowData = [
+    { user: 'C Aravindh', type: 'Message', counter: 'Konathala Venkata', data: 'Hi Buddy', time: '16-06-18 14:22:34' },
+    { user: 'C Aravindh', type: 'Comment', counter: 'Konathala Venkata', data: 'Awesome', time: '16-06-18 15:19:34' },
+    { user: 'Konathala Venkata', type: 'Message', counter: 'C Aravindh', data: 'Hello There', time: '16-06-18 14:23:50' },
+    { user: 'C Aravindh', type: 'Message', counter: 'Konathala Venkata', data: 'How you doing??', time: '16-06-18 14:24:34' },
+    { user: 'Konathala Venkata', type: 'Message', counter: 'C Aravindh', data: 'Good, you?', time: '16-06-18 14:25:34' },
+    { user: 'C Aravindh', type: 'Message', counter: 'Konathala Venkata', data: 'Good. Thanks!', time: '16-06-18 14:26:34' }
+];
+
   constructor(public sharedModule: SharedModule) {
     this.graphData = {
         nodes: [
